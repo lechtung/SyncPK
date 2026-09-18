bab3cade6c52807771be3ce828e76eabcbaba1c0
lechtung <lechtung@gmail.com>
2026-09-18 14:55:22 +0200

    Update app.js

--- server/static/app.js
+++ server/static/app.js
@@ -432,6 +432,27 @@ async function renderHistory(items) {
             let card = document.createElement('div');
             card.className = 'media-card c-card';
             card.id = `card-${item.id}`;
+            card.style.cursor = 'pointer';
+            
+            card.onclick = (e) => {
+                if (e.target.closest('.kebab-menu-btn') || e.target.closest('.kebab-dropdown')) return;
+                
+                let tmdbUrl = '';
+                if (item.media_type === 'movie' && item.tmdb_id) {
+                    tmdbUrl = `https://www.themoviedb.org/movie/${item.tmdb_id}`;
+                } else if (item.media_type === 'episode' && item.tmdb_id) {
+                    // Asumimos que tmdb_id es el id de la serie
+                    tmdbUrl = `https://www.themoviedb.org/tv/${item.tmdb_id}/season/${item.season}/episode/${item.episode}`;
+                } else if (item.media_type === 'movie' && item.title) {
+                    tmdbUrl = `https://www.themoviedb.org/search/movie?query=${encodeURIComponent(item.title)}`;
+                } else if (item.media_type === 'episode' && item.show_title) {
+                    tmdbUrl = `https://www.themoviedb.org/search/tv?query=${encodeURIComponent(item.show_title)}`;
+                }
+                
+                if (tmdbUrl) {
+                    window.open(tmdbUrl, '_blank');
+                }
+            };
             
             let posterStyle = item.poster_path ? `background-image: url('${item.poster_path}')` : 'background-color: #333';
             let bgStyle = item.fanart_path ? `background-image: url('${item.fanart_path}')` : 'background-color: #111';
